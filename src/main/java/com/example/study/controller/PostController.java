269134c3e8f8a4fb88180473dade8f8464dd3f49
lsj8367 <lsj8367@naver.com>
2020-12-30 22:33:40 +0900

    Add POST method & Add lombok

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,31 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") //클래스가 매핑이 겹치는건 상관없음. 메소드만 영향이있다.
+public class PostController {
+
+    // HTML <form>
+    // ajax 검색 등으로 post가 사용됨.
+    // http post body -> data
+    // json, xml, multipart-form (파일올릴때) / text-plain 타입 가능
+
+
+    //@RequestMapping(method = RequestMethod.POST, path = "/postMethod") 이렇게도 사용이 가능함.
+    @PostMapping(value = "/postMethod") //, produces = {"application-json"} 받는방식 지정하는법
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+
+    @PutMapping("/putMethod")
+    public void put(){
+
+    }
+
+    @PatchMapping("/patchMethod")
+    public void patch(){
+
+    }
+}
