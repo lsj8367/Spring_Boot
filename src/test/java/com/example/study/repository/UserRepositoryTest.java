70c247e725eadf3b942c4f0c5b7d10bc79f997fe
lsj8367 <lsj8367@naver.com>
2020-12-31 12:04:46 +0900

    Add read & update

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,8 +4,10 @@ import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
     // Dependency Injection (DI)
@@ -29,12 +31,32 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
-    public void read(){
+    @Test
+    public void read(@RequestParam Long id){
+        //userRepository.findAll(); // select문으로 테이블 전체
+        Optional<User> user = userRepository.findById(2L); //long type이라 2L넣음 아이디타입 2번째
 
+        user.ifPresent(selectUser ->{ //유저에 맞는 아이디가 있으면 결과를 받겠다.
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail()); // 해당유저의 이메일
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        //update user set id=...
+        //JPA에서는 해당 id 존재여부를 확인하고 이 값을 신규로 만들지 않고 업데이트 해준다.
+        user.ifPresent(selectUser ->{ //유저에 맞는 아이디가 있으면 결과를 받겠다.
+            //selectUser.setId(); //이것을 실행하면 여기가 우선으로 작용해서 Optional이 작동하지않음.
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+
+        });
     }
 
     public void delete(){
