e27de92af9bab4a3fd551451dcb3d1a0f5cb5a62
lsj8367 <lsj8367@naver.com>
2021-01-05 19:23:46 +0900

    Add Category read, create

---
+++ src/main/java/com/example/study/service/CategoryApiLogicService.java
@@ -0,0 +1,64 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CRUDInterface;
+import com.example.study.model.entity.Category;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.CategoryApiRequest;
+import com.example.study.model.network.response.CategoryApiResponse;
+import com.example.study.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+public class CategoryApiLogicService implements CRUDInterface<CategoryApiRequest, CategoryApiResponse> {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Header<CategoryApiResponse> create(Header<CategoryApiRequest> request) {
+
+        CategoryApiRequest body = request.getData();
+
+        Category category = Category.builder()
+                .type(body.getType())
+                .title(body.getTitle())
+                .build();
+
+        Category newCategory = categoryRepository.save(category);
+
+
+        return response(newCategory);
+    }
+
+    @Override
+    public Header<CategoryApiResponse> read(Long id) {
+
+        return categoryRepository.findById(id)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<CategoryApiResponse> update(Header<CategoryApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<CategoryApiResponse> response(Category category){
+        CategoryApiResponse body = CategoryApiResponse.builder()
+                .id(category.getId())
+                .type(category.getType())
+                .title(category.getTitle())
+                .build();
+        return Header.OK(body);
+    }
+
+}
